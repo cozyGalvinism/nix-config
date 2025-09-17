6bd24fc5e79abd89f8a879de1d817cef92508eec
cozyGalvinism <jean@der-capta.in>
2025-09-17 14:10:34 +0200

    feat: add zed-editor configuration

--- home/default.nix
+++ home/default.nix
@@ -1,1 +1,1 @@
-{ imports = [ ./fish.nix ./windsurf.nix ./ssh.nix ]; }
+{ imports = [ ./fish.nix ./windsurf.nix ./ssh.nix ./zed.nix ]; }
---
+++ home/zed.nix
@@ -0,0 +1,25 @@
+{ pkgs, ... }:
+
+{
+  programs.zed-editor = {
+    enable = true;
+    package = pkgs.unstable.zed-editor;
+    extensions = [ "nix" ];
+    extraPackages = with pkgs; [ nixd nixfmt-rfc-style unstable.rust-analyzer  ];
+    userSettings = {
+      vim_mode = true;
+      buffer_font_size = 14;
+      buffer_font_family = "CaskaydiaCove Nerd Font";
+      preferred_line_length = 120;
+      format_on_save = "off";
+
+      lsp = {
+        "rust-analyzer" = {
+          binary = {
+            path = "${pkgs.unstable.rust-analyzer}/bin/rust-analyzer";
+          };
+        };
+      };
+    };
+  };
+}
